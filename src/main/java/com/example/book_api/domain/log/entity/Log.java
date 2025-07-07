e08870bbac7da86653648655eb8dc2c4ec1d07d4
LeeHyeonha <lej5769@gmail.com>
2025-07-07 21:04:48 +0900

    feat: Log entity에 User와 연관관계 추가

--- src/main/java/com/example/book_api/domain/log/entity/Log.java
+++ src/main/java/com/example/book_api/domain/log/entity/Log.java
@@ -2,6 +2,7 @@ package com.example.book_api.domain.log.entity;
 
 import com.example.book_api.domain.log.enums.RequestMethod;
 import com.example.book_api.domain.log.enums.TargetType;
+import com.example.book_api.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
@@ -34,9 +35,8 @@ public class Log {
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    // user와 관계 추후 설정
-    // @ManyToOne(fetch = FetchType.LAZY)
-    // private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
 
 
     // 생성자 추후 추가
