a70cefd5d77fc8d255f4d1e37c02c97126e40cdd
taehyeon <max5915@naver.com>
2025-07-07 20:39:51 +0900

    feat(book) : 3계층 구조 적용

--- src/main/java/com/example/book_api/domain/book/controller/BookController.java
+++ src/main/java/com/example/book_api/domain/book/controller/BookController.java
@@ -2,9 +2,11 @@ package com.example.book_api.domain.book.controller;
 
 import com.example.book_api.domain.book.service.BookService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/v1")
+@RestController
+@RequestMapping("/v1")
 @RequiredArgsConstructor
 public class BookController {
 
