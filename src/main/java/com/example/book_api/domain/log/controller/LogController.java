ae02930aa0a6945769a2a6648075e76fa64e0216
LeeHyeonha <lej5769@gmail.com>
2025-07-07 21:15:07 +0900

    feat: Log 3계층 추가

---
+++ src/main/java/com/example/book_api/domain/log/controller/LogController.java
@@ -0,0 +1,11 @@
+package com.example.book_api.domain.log.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/logs")
+public class LogController {
+}
---
+++ src/main/java/com/example/book_api/domain/log/service/LogService.java
@@ -0,0 +1,9 @@
+package com.example.book_api.domain.log.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class LogService {
+}
