a1d79f83c1b9c2e34ee5fff42bce736b460febb7
LeeHyeonha <lej5769@gmail.com>
2025-07-07 20:48:29 +0900

    feat: log entity 추가

---
+++ src/main/java/com/example/book_api/domain/log/entity/Log.java
@@ -0,0 +1,39 @@
+package com.example.book_api.domain.log.entity;
+
+import com.example.book_api.domain.log.enums.RequestMethod;
+import com.example.book_api.domain.log.enums.TargetType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+public class Log {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(value = EnumType.STRING)
+    private TargetType targetType;
+
+    private Long targetId;
+
+    @Enumerated(value = EnumType.STRING)
+    private RequestMethod requestMethod;
+
+    private String requestUri;
+
+    private String message;
+
+    private boolean isSuccess;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    // user와 관계 추후 설정
+    // @ManyToOne(fetch = FetchType.LAZY)
+    // private User user;
+}
---
+++ src/main/java/com/example/book_api/domain/log/enums/TargetType.java
@@ -0,0 +1,5 @@
+package com.example.book_api.domain.log.enums;
+
+public enum TargetType {
+    USER, BOOK, COMMENT, LIKE
+}
