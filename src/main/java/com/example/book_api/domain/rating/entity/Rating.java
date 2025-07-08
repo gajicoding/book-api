b1d6fd09c0174d6b5e83029ea4eeab34d56b2961
hohohosn <day7of01@gmail.com>
2025-07-08 09:21:00 +0900

    feat(9): rating entity

---
+++ src/main/java/com/example/book_api/domain/rating/entity/Rating.java
@@ -0,0 +1,51 @@
+package com.example.book_api.domain.rating.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.awt.print.Book;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ratings", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"book_id", "user_id"})
+})
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Rating {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private int score;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public Rating(int score, Book book, User user) {
+        this.score = score;
+        this.book = book;
+        this.user = user;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void updateScore(int score) {
+        this.score = score;
+        this.updatedAt = LocalDateTime.now();
+    }
+}
