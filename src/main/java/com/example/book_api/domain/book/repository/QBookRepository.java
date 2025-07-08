fa4269036a0a9058e9f9678f5757d73ede394ebb
huouvcti <huouvcti@gamil.com>
2025-07-08 14:59:01 +0900

    feat(Book): 책 카테고리별 top 10 조회 api 구현

--- src/main/java/com/example/book_api/domain/book/repository/QBookRepository.java
+++ src/main/java/com/example/book_api/domain/book/repository/QBookRepository.java
@@ -1,6 +1,7 @@
 package com.example.book_api.domain.book.repository;
 
 import com.example.book_api.domain.book.entity.Book;
+import com.example.book_api.domain.book.enums.CategoryEnum;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -16,11 +17,22 @@ public class QBookRepository {
 
     private final JPAQueryFactory queryFactory;
 
-    public List<Book> findTop10BooksByViewCount() {
+    public List<Book> findTop10Books() {
         return queryFactory.select(book)
                 .from(bookView)
                 .join(bookView.book, book)
                 .groupBy(book)
+                .orderBy(bookView.count().desc())
+                .limit(10)
+                .fetch();
+    }
+
+    public List<Book> findTop10BooksByCategory(CategoryEnum categoryEnum) {
+        return queryFactory.select(book)
+                .from(bookView)
+                .join(bookView.book, book)
+                .groupBy(book)
+                .where(book.category.eq(categoryEnum))
                 .orderBy(bookView.count().desc())
                 .limit(10)
                 .fetch();
--- src/main/java/com/example/book_api/domain/book/service/BookService.java
+++ src/main/java/com/example/book_api/domain/book/service/BookService.java
@@ -1,6 +1,7 @@
 package com.example.book_api.domain.book.service;
 
 import com.example.book_api.domain.book.dto.BookResponseDto;
+import com.example.book_api.domain.book.enums.CategoryEnum;
 import com.example.book_api.domain.book.repository.BookRepository;
 import com.example.book_api.domain.book.repository.QBookRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,9 +16,19 @@ public class BookService {
     private final BookRepository bookRepository;
     private final QBookRepository qBookRepository;
 
-    // 책 전체 top
+    // 책 전체 top 10
     public List<BookResponseDto> getTopBooks() {
-        return qBookRepository.findTop10BooksByViewCount()
+        return qBookRepository.findTop10Books()
+                .stream()
+                .map(BookResponseDto::new)
+                .toList();
+    }
+
+    // 책 카테고리별 top 10
+    public List<BookResponseDto> getTopBookByCategory(String category) {
+        CategoryEnum categoryEnum = CategoryEnum.of(category).orElseThrow(RuntimeException::new);
+
+        return qBookRepository.findTop10BooksByCategory(categoryEnum)
                 .stream()
                 .map(BookResponseDto::new)
                 .toList();
