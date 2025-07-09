cc18f3fd2837ef35a427e638b14ee1f80c9ec6de
LeeHyeonha <lej5769@gmail.com>
2025-07-09 15:23:53 +0900

    Feat: saveLog 로그 저장 기능 추가

--- src/main/java/com/example/book_api/domain/log/service/LogService.java
+++ src/main/java/com/example/book_api/domain/log/service/LogService.java
@@ -1,9 +1,20 @@
 package com.example.book_api.domain.log.service;
 
+import com.example.book_api.domain.log.entity.Log;
+import com.example.book_api.domain.log.repository.LogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class LogService {
+
+    private final LogRepository logRepository;
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void saveLog(Log log){
+        logRepository.save(log);
+    }
 }
