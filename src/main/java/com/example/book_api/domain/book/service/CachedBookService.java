3b7fd39f705816d620d1dbfe4e4ff8a3861ad166
huouvcti <huouvcti@gamil.com>
2025-07-10 13:16:58 +0900

    feat(Book): 테스트 코드 작성 및 코드 리팩토링

--- src/main/java/com/example/book_api/domain/book/service/CachedBookService.java
+++ src/main/java/com/example/book_api/domain/book/service/CachedBookService.java
@@ -1,7 +1,6 @@
 package com.example.book_api.domain.book.service;
 
 import com.example.book_api.domain.book.dto.BookResponseDto;
-import com.example.book_api.domain.book.validation.BookValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
@@ -10,10 +9,9 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class CachedBoardService {
+public class CachedBookService {
 
     private final BookService bookService;
-    private final BookValidator bookValidator;
 
     @Cacheable(value = "bookTop", key = "'all'")
     public List<BookResponseDto> getTopBooksCached() {
@@ -22,15 +20,11 @@ public class CachedBoardService {
 
     @Cacheable(value = "bookTop", key = "'category_' + #category")
     public List<BookResponseDto> getTopBookByCategoryCached(String category) {
-        bookValidator.validateCategory(category);
-
         return bookService.getTopBookByCategory(category);
     }
 
     @Cacheable(value = "bookTop", key = "'ageGroup_' + #ageGroup")
     public List<BookResponseDto> getTopBookByUserAgeCached(String ageGroup) {
-        bookValidator.validateAgeGroup(ageGroup);
-
         return bookService.getTopBookByUserAge(ageGroup);
     }
 }
---
+++ src/test/java/com/example/book_api/domain/book/unit/controller/BookControllerTest.java
@@ -0,0 +1,4 @@
+package com.example.book_api.domain.book.unit.controller;
+
+public class BookControllerTest {
+}
