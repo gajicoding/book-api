b42c3d57776f04e5caa6e8c874bf2956df0115af
mong9ssi <mong9ssi@naver.com>
2025-07-10 10:48:33 +0900

    feat : GlobalExceptionHandler MethodArgumentNotValidException 추가
    UserService에 findById 추가

--- src/main/java/com/example/book_api/domain/user/service/UserService.java
+++ src/main/java/com/example/book_api/domain/user/service/UserService.java
@@ -9,7 +9,6 @@ import com.example.book_api.domain.user.exception.UserException;
 import com.example.book_api.domain.user.repository.UserRepository;
 import com.example.book_api.global.config.PasswordEncoder;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
