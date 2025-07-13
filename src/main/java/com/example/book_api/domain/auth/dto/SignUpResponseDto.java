5581d4090b7fcd9f025225f8bbf0d8dbb34a62f0
huouvcti <huouvcti@gamil.com>
2025-07-13 16:30:37 +0900

    refactor(auth): 회원가입, 로그인 응답 형식 병경

--- src/main/java/com/example/book_api/domain/auth/dto/SignUpResponseDto.java
+++ src/main/java/com/example/book_api/domain/auth/dto/SignUpResponseDto.java
@@ -1,10 +1,13 @@
 package com.example.book_api.domain.auth.dto;
 
+import com.example.book_api.domain.user.entity.User;
+import com.example.book_api.domain.user.enums.Role;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @Getter
@@ -16,5 +19,18 @@ public class SignUpResponseDto {
     private final String email;
     private final String name;
     private final LocalDate birth;
+    private final Role role;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+    public SignUpResponseDto(User user) {
+        id = user.getId();
+        email = user.getEmail();
+        name = user.getName();
+        birth = user.getBirth();
+        role = user.getRole();
+        createdAt = user.getCreatedAt();
+        modifiedAt = user.getModifiedAt();
+    }
 
 }
